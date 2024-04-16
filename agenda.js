7c10f2613b18919fd16a2ba2c61d7276158cdf63
Hugo Cremers <hcremers@student.s19.be>
2024-04-16 11:46:17 +0200

    code cleaned

--- agenda.js
+++ agenda.js
@@ -1,81 +1,81 @@
 import "colors"
 
-const main = async (begin_at, end_at, token42, tokenGoogle) => {
-	async function getEvents(exam) {
-		const myHeaders = new Headers();
-		myHeaders.append("Authorization", `Bearer ${token42}`);
+async function getEvents(exam) {
+	const myHeaders = new Headers();
+	myHeaders.append("Authorization", `Bearer ${token42}`);
 
-		const requestOptions = {
-			method: "GET",
-			headers: myHeaders,
-			redirect: "follow"
-		};
+	const requestOptions = {
+		method: "GET",
+		headers: myHeaders,
+		redirect: "follow"
+	};
 
-		console.log(`Getting ${exam ? 'exams' : 'events'}...`.blue)
-		const result = await fetch(`https://api.intra.42.fr/v2/campus/12/${exam ? 'exams' : 'events'}?range[begin_at]=${begin_at}, ${end_at}`, requestOptions)
-			.then((response) => response.json())
-			.then((result) => result)
-			.catch((error) => console.error(error));
+	console.log(`Getting ${exam ? 'exams' : 'events'}...`.blue)
+	const result = await fetch(`https://api.intra.42.fr/v2/campus/12/${exam ? 'exams' : 'events'}?range[begin_at]=${begin_at}, ${end_at}`, requestOptions)
+		.then((response) => response.json())
+		.then((result) => result)
+		.catch((error) => console.error(error));
 
-		if (result.error) {
-			console.error(`GET Error ${result.status}: `.red + result.error);
-			return null;
-		}
-		else {
-			const ret = [];
-			for (let i = 0; i < result.length; i++) {
-				ret.push({
-					"summary": result[i].name,
-					"description": result[i].description,
-					"location": result[i].location,
-					"start": {
-						"dateTime": result[i].begin_at
-					},
-					"end": {
-						"dateTime": result[i].end_at
-					},
-					"colorId": `${exam ? '4' : '5'}`
-				});
-				/*
-					'1' Lavender
-					'2' Sage
-					'3' Grape
-					'4' Flamingo
-					'5' Banana
-					'6' Tangerine
-					'7' Peacock
-					'8' Graphite
-					'9' Blueberry
-					'10' Basil
-					'11' Tomato
-				*/
-			}
-			return ret;
-		}
+	if (result.error) {
+		console.error(`GET Error ${result.status}: `.red + result.error);
+		return null;
 	}
-
-	async function newPostEvents(events) {
-		const myHeaders = new Headers();
-		myHeaders.append("Content-Type", "application/json");
-		myHeaders.append("Authorization", `Bearer ${tokenGoogle}`);
-
-		const raw = JSON.stringify(events);
-
-		const requestOptions = {
-			method: "POST",
-			headers: myHeaders,
-			body: raw,
-			redirect: "follow"
-		};
-
-		return fetch("https://www.googleapis.com/calendar/v3/calendars/ion@s19.be/events", requestOptions)
-			.then((response) => response.json())
-			.then((result) => {
-				return result;
-			})
-			.catch((error) => console.error(error));
+	else {
+		const ret = [];
+		for (let i = 0; i < result.length; i++) {
+			ret.push({
+				"summary": result[i].name,
+				"description": result[i].description,
+				"location": result[i].location,
+				"start": {
+					"dateTime": result[i].begin_at
+				},
+				"end": {
+					"dateTime": result[i].end_at
+				},
+				"colorId": `${exam ? '4' : '5'}`
+			});
+			/*
+				'1' Lavender
+				'2' Sage
+				'3' Grape
+				'4' Flamingo
+				'5' Banana
+				'6' Tangerine
+				'7' Peacock
+				'8' Graphite
+				'9' Blueberry
+				'10' Basil
+				'11' Tomato
+			*/
+		}
+		return ret;
 	}
+}
 
+async function newPostEvents(events) {
+	const myHeaders = new Headers();
+	myHeaders.append("Content-Type", "application/json");
+	myHeaders.append("Authorization", `Bearer ${tokenGoogle}`);
+
+	const raw = JSON.stringify(events);
+
+	const requestOptions = {
+		method: "POST",
+		headers: myHeaders,
+		body: raw,
+		redirect: "follow"
+	};
+
+	return fetch("https://www.googleapis.com/calendar/v3/calendars/ion@s19.be/events", requestOptions)
+		.then((response) => response.json())
+		.then((result) => {
+			return result;
+		})
+		.catch((error) => console.error(error));
+}
+
+const main = async () => {
 	const events = await getEvents(0)
 	if (!events) return null
 	const exams = await getEvents(1)
@@ -83,7 +83,6 @@ const main = async (begin_at, end_at, token42, tokenGoogle) => {
 
 	for (let i = 0; i < exams.length; i++) { events.push(exams[i]) }
 
-	// console.log(events)
 	if (!events.length) {
 		console.log("No events to create.".yellow)
 		return null
@@ -107,6 +106,6 @@ const token42 = err1 ? '' : 'db812164ba193432951ca26fba15c03d5f61e6ea26377b20c10
 const err2 = 0
 const tokenGoogle = err2 ? '' : 'ya29.a0Ad52N39_wXuICQQCJBAcPXaYIvz1-EjaXAOCoMKFt0jyH2UpUkYYUIIT-3rAPMb7RQswstmcT9FWewoVCw_jK_6IaUeET-O4IpvFxNviWIpk9Rxb948Y1Azya8tT94wJ7PV5ndzjRzbm3CV_JY_Hnoc38DV7Hk7J6LsoaCgYKAfISARASFQHGX2MiRXAaYFicRQehm8HPdrIR7Q0171'
 const begin_at = '2024-04-15'
-const end_at = '2024-04-22'
+const end_at = '2024-04-22' // Excluded from scope, so count 1 more day
 
-main(begin_at, end_at, token42, tokenGoogle)
+main()

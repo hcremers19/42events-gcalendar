952c7f09a95bf5a20b01fe3e7e0bd6e742fba69e
Hugo Cremers <hcremers@student.s19.be>
2024-05-13 15:47:38 +0200

    moved token generation to main

--- index.js
+++ index.js
@@ -3,12 +3,22 @@ import { get, getToken } from "./api/42.js";
 import { insert } from "./api/gcal.js";
 
 async function main() {
+	const token_42 = await getToken();
+	if (!token_42) return null;
+
+	const params = {
+		CALENDAR_ID: process.env.CALENDAR_ID,
+		TOKEN_42: err1 ? '' : token_42,
+		TOKEN_GOOGLE: err2 ? '' : process.env.TOKEN_GOOGLE,
+		begin_at: process.env.BEGIN_AT,
+		end_at: process.env.END_AT,
+	}
+
 	const events = await get('events', params);
-	if (!events)
-		return null;
+	if (!events) return null;
+
 	const exams = await get('exams', params);
-	if (!exams)
-		return null;
+	if (!exams) return null;
 
 	for (let i = 0; i < exams.length; i++)
 		events.push(exams[i]);
@@ -17,7 +27,7 @@ async function main() {
 		console.log("No events to create.".yellow);
 		return null;
 	}
-	console.log("GET OK!".green);
+	console.log("[42] ".grey + "GET OK!".green);
 
 	let i = 0;
 	while (i < events.length) {
@@ -27,20 +37,10 @@ async function main() {
 		else
 			i++;
 	}
-	console.log(`Updated ${i} event${i === 1 ? '' : 's'}.`.blue)
+	console.log("[Google] ".grey + `Updated ${i} event${i === 1 ? '' : 's'}.`.blue)
 }
 
 const err1 = 0
 const err2 = 0
-
-const token_42 = await getToken()
-
-const params = {
-	CALENDAR_ID: process.env.CALENDAR_ID,
-	TOKEN_42: err1 ? '' : token_42,
-	TOKEN_GOOGLE: err2 ? '' : process.env.TOKEN_GOOGLE,
-	begin_at: process.env.BEGIN_AT,
-	end_at: process.env.END_AT,
-}
 
 main()
